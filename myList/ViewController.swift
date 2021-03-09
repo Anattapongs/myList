9071f9b1534233292b27298345013aa043711e03
Anattapong Suktep <benzanattapong@Anattapongs-MacBook-Pro.local>
2021-03-09 11:46:34 +0700

    feat: setup table view

--- myList/ViewController.swift
+++ myList/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "The List"
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+       
     }
 
+    @IBAction func addNameButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =
+          tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                        for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
+    
 }
 
